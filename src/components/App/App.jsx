e9edddd7f18242eac0efb368a8be3f1a1be679f8
vladislav-slobodenjuk <vladjun@gmail.com>
2023-07-29 22:06:23 +0300

    redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
 
 import { Container } from './App.styled';
@@ -6,67 +7,80 @@ import { ContactForm } from '../ContactForm/ContactForm';
 import { ContactList } from '../ContactList/ContactList';
 import { Filter } from '../Filter/Filter';
 
-const STORAGE_KEY = 'contacts';
+import { getContacts, getFilter } from 'redux/selectors';
+import { addContact, deleteContact } from 'redux/contactsSlice';
+import { setFilterContacts } from 'redux/filterSlice';
 
-const initContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  { id: 'id-5', name: 'Vladislav Sl', number: '333-55-44' },
-];
+// const STORAGE_KEY = 'contacts';
+
+// const initContacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//   { id: 'id-5', name: 'Vladislav Sl', number: '333-55-44' },
+// ];
 
 // localStorage.setItem(STORAGE_KEY, JSON.stringify(initContacts));
 
 export const App = () => {
-  const [contacts, setContacts] = useState(initContacts);
-  const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useState(initContacts);
+  // const [filter, setFilter] = useState('');
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const dispatch = useDispatch();
 
   const handleFilterChange = ({ target }) => {
-    setFilter(target.value);
+    // setFilter(target.value);
+    dispatch(setFilterContacts(target.value));
   };
 
-  const addContact = ({ name, number }) => {
+  const handleAddContact = ({ name, number }) => {
     const isExist = contacts.find(contact => contact.name === name);
     if (isExist) return alert(`${name} is already in contacts.`);
 
-    setContacts([{ name, number, id: nanoid() }, ...contacts]);
+    // setContacts([{ name, number, id: nanoid() }, ...contacts]);
+    dispatch(addContact({ name, number }));
   };
 
   const filterContacts = () => {
     const normalizedFilter = filter.toLowerCase();
-
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  const deleteContact = id => {
-    const restContacts = contacts.filter(contact => contact.id !== id);
-    setContacts(restContacts);
+  const handleDeleteContact = id => {
+    // const restContacts = contacts.filter(contact => contact.id !== id);
+    // setContacts(restContacts);
+    dispatch(deleteContact(id));
   };
 
-  useEffect(() => {
-    const savedContacts = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    // console.log('savedContacts', savedContacts);
-    if (savedContacts) setContacts(savedContacts);
-  }, []);
+  // useEffect(() => {
+  //   const savedContacts = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  //   // console.log('savedContacts', savedContacts);
+  //   if (savedContacts) setContacts(savedContacts);
+  // }, []);
 
-  useEffect(() => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
-    // console.log('second');
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
+  //   // console.log('second');
+  // }, [contacts]);
 
   const filteredContacts = filterContacts();
 
   return (
     <Container>
       <h1>Phonebook</h1>
-      <ContactForm onHandleSubmit={addContact} />
+      <ContactForm onHandleSubmit={handleAddContact} />
 
       <h2>Contacts</h2>
       <Filter filterValue={filter} onFilterChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} onDeleteClick={deleteContact} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteClick={handleDeleteContact}
+      />
     </Container>
   );
 };
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const contactsInitialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  { id: 'id-5', name: 'Vladislav Sl', number: '333-55-44' },
+];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare({ name, number }) {
+        return {
+          payload: { name, number, id: nanoid() },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      // const index = state.findIndex(contact => contact.id === action.payload);
+      // state.splice(index, 1);
+
+      state = state.filter(contact => contact.id !== action.payload);
+      return state;
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
